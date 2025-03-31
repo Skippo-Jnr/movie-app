ad4fdc2adda70854164692d12af419008c71e57c
Skippo Jnr♥️🏀🏀 <skippojnr@outlook.com>
2025-03-31 12:08:51 +0000

    Fetch movies from API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
-  const movies = [
-    { id: 1, title: "JOhn Wick", release_date: "2020" },
-    { id: 2, title: "Terminator", release_date: "2023" },
-    { id: 3, title: "The Matrix", release_date: "2025" },
-  ];
+  const [movies, setMovies] = useState([]); // this is the state of the movies
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []); // this is the useEffect hook that runs when the component mounts
 
   const handleSearch = (e) => {
     e.preventDefault();
---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+const API_KEY = "";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch popular movies");
+  }
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+
+  const data = await response.json();
+  return data.results;
+};
