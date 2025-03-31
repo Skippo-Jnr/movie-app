daf8360c3a382fc1795f237dd817818980023fad
Skippo Jnr♥️🏀🏀 <skippojnr@outlook.com>
2025-03-31 12:15:15 +0000

    Show posters of movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,10 @@ function MovieCard({ movie }) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
             ♥
@@ -17,7 +20,8 @@ function MovieCard({ movie }) {
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date?.split("-")[0]}</p>
+        {/* // to show the year without the month and date */}
       </div>
     </div>
   );
