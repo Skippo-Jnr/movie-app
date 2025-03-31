c467a8cd73812c17ec10d32c1d15d73c7ffe3d6e
Skippo Jnr♥️🏀🏀 <skippojnr@outlook.com>
2025-03-31 11:41:47 +0000

    Route between pages and create NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favorite from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />{" "}
+          {/* displays the Home Page element  when we use '/'  */}
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favourites Movies Yet</h2>
+      <p>Start adding movies too your favourites and they will appear here</p>
+    </div>
+  );
+}
+
+export default Favorite;
